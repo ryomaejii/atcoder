5a4613f5db4464445a4be1a0764d1d3f47bb3f74
ryomaejii <82492270+ryomaejii@users.noreply.github.com>
2023-10-23 09:53:20 +0900

    Answer abc325

---
+++ abc325/a/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; (i) < (n); ++(i))
+
+string S, T;
+
+int main()
+{
+  cin >> S >> T;
+  cout << S << " "
+       << "san" << endl;
+}
