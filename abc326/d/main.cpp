d717834c394c349cbd1fe3dc1b7d74ec76815410
ryomaejii <82492270+ryomaejii@users.noreply.github.com>
2023-10-28 22:46:45 +0900

    I could not answer abc326/d

---
+++ abc326/d/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, j, n) for (int i = j; (i) < (n); ++(i))
+
+int N;
+string R, C;
+
+int main()
+{
+  cin >> N;
+  cin >> R;
+  cin >> C;
+
+  // まず、端っこに数字を貼り付ける。
+}
